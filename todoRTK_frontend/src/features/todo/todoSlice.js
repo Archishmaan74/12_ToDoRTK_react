090a6fabee10ec950e568aeb04920128da716bfd
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2025-06-08 18:53:27 +0530

    created todoSlice and added add reducer to it

---
+++ todoRTK_frontend/src/features/todo/todoSlice.js
@@ -0,0 +1,16 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: 1, textValue: "Hello World" }],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = { id: nanoid(), textValue: action.payload };
+      state.todos.push(todo);
+    },
+  },
+});
